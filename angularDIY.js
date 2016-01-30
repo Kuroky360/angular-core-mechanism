d3cbe13f6401b3198a73178e3f41f50ff7c1cb9e
Wangzg <wangzg1104@gmail.com>
2016-01-30 23:21:47 +0800

    add forEachSorted fn

--- angularDIY.js
+++ angularDIY.js
@@ -118,6 +118,14 @@
         return parseInt(value,10);
     }
 
+    function forEachSorted(obj,iterator,context){
+        var keys=Object.keys(obj).sort();
+        for(var i= 0;i<keys.length;i++){
+            iterator.call(context,obj[keys[i]],keys[i]);
+        }
+        return keys;
+    }
+
     function isArray(value){ return Array.isArray(value);}
 
     // check RegExp
