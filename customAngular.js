24d79a96c08b30b0230b3b430db073d61a50ef8d
Wangzg <wangzg1104@gmail.com>
2016-03-15 21:33:27 +0800

    add `all` method for $Q

--- customAngular.js
+++ customAngular.js
@@ -883,6 +883,31 @@
             return d;
         };
 
+        // combines multiple promises into a single promise that is resolved when all of the input promises are resolved.
+        function all(promises){
+            var deferred=new Deferred(),
+                counter= 0,
+                results=isArray(promises)?[]:{};
+
+            forEach(promises,function(promise,key){
+                counter++;
+                when(promise).then(function (value) {
+                    if(results.hasOwnProperty(key)) return;
+                    results[key]=value;
+                    if(!(--counter)) deferred.resolve(results);
+                }, function (reason) {
+                    if(results.hasOwnProperty(key)) return;
+                    deferred.reject(reason);
+                });
+            });
+
+            if(counter===0){
+                deferred.resolve(results);
+            }
+
+            return deferred.promise;
+        }
+
         // make the instanceof operator work for promises.
         $Q.prototype=Promise.prototype;
 
@@ -890,6 +915,7 @@
         $Q.defer=defer;
         $Q.when=when;
         $Q.resolve=resolve;
+        $Q.all=all;
 
         return $Q;
     }
