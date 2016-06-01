6ad6b77621a83435b72740373bdef7d41c4af867
kuroky360 <wangzg1104@163.com>
2016-06-01 18:18:21 +0800

    Make annotate fn work

--- customAngular.js
+++ customAngular.js
@@ -567,10 +567,22 @@
                 });
             }])
     }
+    var ARROW_ARG = /^([^\(]+?)=>/;
+    var FN_ARGS = /^[^\(]*\(\s*([^\)]*)\)/m;
+    var FN_ARG_SPLIT = /,/;
+    var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+    var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+    function extractArgs(fn){
+        var fnText=fn.toString().replace(STRIP_COMMENTS,''),
+            args=fnText.match(ARROW_ARG)||fnText.match(FN_ARGS);
+        return args;
+    }
 
     // annotate fn
     function annotate(fn,strictDi,name) {
         var $inject,
+            argDecl,
             last;
         if(isFunciton(fn)){
             if(!($inject=fn.$inject)){
@@ -580,14 +592,20 @@
                         throw $injectorMinErr('strictdi',
                         '{0} is not using explicit annotation and cannot be invoked in strict mode',name);
                     }
-                    //todo
+                    argDecl=extractArgs(fn);
+                    forEach(argDecl[1].split(FN_ARG_SPLIT),function(arg){
+                        arg.replace(FN_ARG,function(all,underscore,name){
+                            $inject.push(name);
+                        });
+                    });
                 }
                 fn.$inject=$inject;
             }
         }else if(isArray(fn)){
-
+            last=fn.length-1;
+            $inject=fn.slice(0,last);
         }else{
-
+            // todo
         }
 
         return $inject;
